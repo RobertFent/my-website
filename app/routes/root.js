8164a5d21a5bd9b672db2d232d1368b428c4fe05
Robert Fent <robert@eco2grow.com>
2025-07-11 08:31:20 +0200

    Fixed contact entries not shown at initial get to root

--- app/routes/root.js
+++ app/routes/root.js
@@ -5,14 +5,13 @@ import { Root } from '../views/Root.js';
  * @param {{ app: import("fastify").FastifyInstance, db: import("better-sqlite3").Database }}
  */
 export const init = async ({ app, db }) => {
-	// initially load contact entries
-	const contactEntries = db
-		.prepare(
-			sql`SELECT * FROM contact_entries ORDER BY timestamp DESC LIMIT 5 `
-		)
-		.all();
-	console.log(contactEntries);
 	app.get('/', async (_, reply) => {
+		// initially load contact entries
+		const contactEntries = db
+			.prepare(
+				sql`SELECT * FROM contact_entries ORDER BY timestamp DESC LIMIT 5 `
+			)
+			.all();
 		return reply.render(Root, { entries: contactEntries });
 	});
 };
